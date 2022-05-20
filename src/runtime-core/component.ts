7f8468de909fe8ea2b25b92888da69a5b298269a
Calvin <icetgale@gmail.com>
2022-05-20 16:58:28 +0800

    feat: getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -35,9 +35,11 @@ function setupStatefulComponent(instance: any) {
   instance.proxy = new Proxy({_: instance}, PublicInstanceProxyHandlers)
 
   if (setup) {
+    setCurrentInstance(instance);
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit
     })
+    setCurrentInstance(null);
     handleSetupResult(instance, setupResult);
   }
 }
@@ -58,4 +60,14 @@ function finishComponentSetup(instance: any) {
   // if (Component.render) {
   instance.render = Component.render;
   // }
+}
+
+let currentInstance = null
+
+function setCurrentInstance(instance) {
+  currentInstance = instance
+}
+
+export function getCurrentInstance() {
+  return currentInstance
 }
